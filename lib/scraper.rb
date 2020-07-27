9c3402dd8f4ee09e9b95eaeeb63ba33ea69823e1
Amyre Polk <myremyre101@icloud.com>
2020-07-27 23:38:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,44 +1,38 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    a = open(index_url)
+        doc = Nokogiri::HTML(a)
+        b = []
+        doc.css('.student-card').each do |elem|
+          b << {:name => "#{elem.css('h4.student-name').text}", :location => "#{elem.css('p.student-location').text}", :profile_url => "#{elem.css('a').first["href"]}"}
+        end
+        b
+      end
 
-  doc = Nokogiri::HTML(open(index_url))
-   students = []
-   doc.css("div.roster-cards-container").each do |card|
-     card.css(".student-card a").each do |student|
-       student_name = student.css('.student-name').text
-       student_location = student.css('.student-location').text
-       student_profile_url = "#{student.attr('href')}"
-       students << {name: student_name, location: student_location, profile_url: student_profile_url}
-     end
-   end
-   students
- end
+      def self.scrape_profile_page(profile_url)
+        doc = Nokogiri::HTML(open(profile_url))
+    attributes = doc.css('.social-icon-container a')
+    b = {}
+    attributes.each do |att|
+      if att['href'].include?("twitter")
+        b[:twitter] = att['href']
+      elsif att['href'].include?("linkedin")
+        b[:linkedin] = att['href']
+      elsif att['href'].include?("github")
+        b[:github] = att['href']
+      else
+        b[:blog] = att['href']
+      end
+    end
+    b[:profile_quote] = doc.css('.profile-quote').text
+    b[:bio] = doc.css('.bio-content p').first.text
+    b
+
 
   end
-
-  def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(open(profile_url))
- scraped_student = {}
- doc.css("div.social-icon-container").children.css("a").each do |social_media_link|
-   temp_link = "#{social_media_link.attr('href')}"
-   if temp_link.include?("linkedin")
-     linked_in = temp_link
-     scraped_student[:linkedin] = linked_in
-   elsif temp_link.include?("twitter")
-     twitter = temp_link
-     scraped_student[:twitter] = twitter
-   elsif temp_link.include?("github")
-     github = temp_link
-     scraped_student[:github] = github
-   else
-     scraped_student[:blog] = temp_link
-   end
- end
- scraped_student[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text if doc.css("div.bio-content.content-holder div.description-holder p")
- scraped_student[:profile_quote] = doc.css("div.vitals-text-container div.profile-quote").text
- scraped_student
 end
