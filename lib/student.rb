3eb134e1fb0b4bb98b084bcbded136103d20fe48
Amyre Polk <myremyre101@icloud.com>
2020-07-27 23:35:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,25 +1,27 @@
-class Student
+def initialize(student_hash)
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
-
-  @@all = []
-
-  def initialize(student_hash)
-    student_hash.each{|key,value| self.send(("#{key}="), value)}
-   @@all << self
+    @student_hash = student_hash
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
-    students_array.each{|student| Student.new(student)}
+
+    students_array.each do |student|
+      Student.new(student)
+    end 
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |key,value|
-     self.send(("#{key}="), value)
+
+    attributes_hash.each do |key, value|
+      self.send(("#{key}="), value)
+    end 
   end
 
   def self.all
+
     @@all
   end
-end
 end
